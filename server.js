1dd73342fd3227931cacb446e526290b5aa0bc91
Victor Volovik <VictorVolovik@users.noreply.github.com>
2021-01-19 20:08:35 +0300

    Add getting GitHub user public repos number

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 const express = require("express");
+const axios = require("axios");
 
 const PORT = process.env.PORT || 5000;
 
+const API_GET_GITHUB_USER = username => `https://api.github.com/users/${username}`;
+
 const app = express();
 
-app.get("*", (req, res) => res.send("OK"));
+// Request data from GitHub
+async function getReposNum (req, res, next) {
+  try {
+    console.log("Fetching data...");
+
+    const { username } = req.params;
+
+    const response = await axios.get(API_GET_GITHUB_USER(username))
+    const { public_repos: reposNum } = response.data;
+
+    res.send(`<h2>${username} has ${reposNum} public repositories.</h2>`);
+
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(500);
+  }
+}
+
+app.get("/repos_number/:username", getReposNum);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
